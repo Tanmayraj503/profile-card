4843e97ef276c9896ced75ce0c17cefc81b081ec
Tanmay Raj <tanmayraj707@gmail.com>
2025-08-13 16:14:00 +0530

    commit

---
+++ script.js
@@ -0,0 +1,5 @@
+const toggle = document.getElementById("toggle-btn");
+const social = document.querySelector(".social");
+toggle.addEventListener("click", () => {
+social.classList.toggle("show");
+});
